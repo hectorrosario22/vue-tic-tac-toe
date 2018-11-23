a8c5acb977e2b0953a09a491dd6b3221d9b4aa55
Héctor Rosario <he.rosario@dga.gov.do>
2018-11-23 15:43:52 -0400

    Initial Commit

---
+++ src/components/Cell.vue
@@ -0,0 +1,70 @@
+<template>
+  <td class="cell" @click="strike">{{ mark }}</td>
+</template>
+
+<script>
+export default {
+  props: [
+    'name'
+  ],
+  data() {
+    return {
+      frozen: false,
+      mark: ''
+    }
+  },
+  created () {
+    window.eventBus.$on('freeze', () => this.frozen = true)
+    window.eventBus.$on('clearCell', () => {
+      this.mark = ''
+      this.frozen = false
+    })
+  },
+  methods: {
+    strike () {
+      if (!this.frozen) {
+        this.mark = this.$parent.activePlayer
+        this.frozen = true
+        window.eventBus.$emit('strike', this.name)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.cell {
+  width: 33.333%;
+  height: 90px;
+  border: 6px solid #2c3e50;
+  font-size: 3.5em;
+  font-family: "Gochi Hand", sans-serif;
+}
+
+.cell:hover {
+  background-color: #7f8c8d;
+}
+
+.cell::after {
+  content: "";
+  display: block;
+}
+
+.cell:first-of-type {
+  border-left-color: transparent;
+  border-top-color: transparent;
+}
+
+.cell:nth-of-type(2) {
+  border-top-color: transparent;
+}
+
+.cell:nth-of-type(3) {
+  border-right-color: transparent;
+  border-top-color: transparent;
+}
+
+tr:nth-of-type(3) .cell {
+  border-bottom-color: transparent;
+}
+</style>
